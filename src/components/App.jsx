31fbbb1b5752429ab756f392e8dda4c4ccc09b8d
Ben Stoltz <bstoltz@esri.com>
2015-11-03 15:59:28 -0500

    Require to import
    Swapped require blocks to imports.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,12 @@ import MessageList from './MessageList.jsx';
 import ChannelList from './ChannelList.jsx';
 import MessageBox from './MessageBox.jsx';
 //import mui from 'material-ui';
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
+import personalTheme from './theme-palette';
+import AppBar from 'material-ui/lib/app-bar';
 
 
-const personalTheme = require('./theme-palette.js');
-const ThemeManager = require('material-ui/lib/styles/theme-manager');
-const LightRawTheme = require('material-ui/lib/styles/raw-themes/light-raw-theme');
-let AppBar = require('material-ui/lib/app-bar');
 
 
 class App extends React.Component {
