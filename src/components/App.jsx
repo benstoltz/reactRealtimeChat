e0bfa0c2cd5f1fff7ce5e894b26490c2eece753a
Ben Stoltz <bstoltz@esri.com>
2015-11-03 14:46:41 -0500

    Material UI & Firebase added
    Added firebase and material UI to application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,50 @@
 import React from 'react';
+import MessageList from './MessageList.jsx';
+import ChannelList from './ChannelList.jsx';
+import MessageBox from './MessageBox.jsx';
+//import mui from 'material-ui';
+
+
+const personalTheme = require('./theme-palette.js');
+const ThemeManager = require('material-ui/lib/styles/theme-manager');
+const LightRawTheme = require('material-ui/lib/styles/raw-themes/light-raw-theme');
+let AppBar = require('material-ui/lib/app-bar');
 
 
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {
-            messages: [
-                'Hullo, there, how are you?',
-                'I am fine, and yourself?'
-            ]
+    }
+
+    static childContextTypes = {
+        muiTheme: React.PropTypes.object
+    };
+
+
+
+    getChildContext() {
+        return {
+            muiTheme: ThemeManager.getMuiTheme(personalTheme)
         };
     }
 
     render() {
-        var messageNodes = this.state.messages.map((message) => {
-            return (
-                <div>{message}</div>
-            );
-        });
-
-        return (
-            <div>{messageNodes}</div>
-        );
+       return (
+           <div>
+               <AppBar title="Awesome Chat App" />
+               <div style={{
+                display: 'flex',
+                flexFlow: 'row',
+                maxWidth: '1200',
+                width: '100%',
+                margin: '30px auto 30px'
+               }}>
+                   <ChannelList />
+                   <MessageList />
+               </div>
+               <MessageBox />
+           </div>
+       );
     }
 }
 
