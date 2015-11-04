10cca28012a322af9263b1fdb434c53050316b44
benstoltz <stoltz.ben@me.com>
2015-11-04 01:07:26 -0500

    Github Login added
    Signed-off-by: benstoltz <stoltz.ben@me.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import MessageList from './MessageList.jsx';
 import ChannelList from './ChannelList.jsx';
 import MessageBox from './MessageBox.jsx';
+import Login from './Login.jsx';
 //import mui from 'material-ui';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
-import personalTheme from './theme-palette';
+//import personalTheme from './theme-palette';
 import AppBar from 'material-ui/lib/app-bar';
+import connectToStores from 'alt/utils/connectToStores';
+import ChatStore from '../stores/ChatStore';
+import createFragment from 'react-addons-create-fragment';
 
+const personalTheme = require('./theme-palette');
 
-
-
+@connectToStores
 class App extends React.Component {
     constructor() {
         super();
+    }
+
+    static getStores() {
+        return [ChatStore];
+    }
+
+    static getPropsFromStores() {
+        return ChatStore.getState();
     }
 
     static childContextTypes = {
@@ -29,20 +41,32 @@ class App extends React.Component {
     }
 
     render() {
+        let view;
+
+        if (this.props.user) {
+            view = (
+                <div>
+                    <div style={{
+                        display: 'flex',
+                        flexFlow: 'row',
+                        maxWidth: '1200',
+                        width: '100%',
+                        margin: '30px auto 30px'
+                    }}>
+                        <ChannelList />
+                        <MessageList />
+                    </div>
+                    <MessageBox />
+                </div>
+            );
+        } else {
+            view = <Login />;
+        }
+
        return (
            <div>
                <AppBar title="Awesome Chat App" />
-               <div style={{
-                display: 'flex',
-                flexFlow: 'row',
-                maxWidth: '1200',
-                width: '100%',
-                margin: '30px auto 30px'
-               }}>
-                   <ChannelList />
-                   <MessageList />
-               </div>
-               <MessageBox />
+               {view}
            </div>
        );
     }
